562d324562798b57c42eda6bf58a1c999721ea1d
Bejgu Leonard Mihai <61775738+Leobej@users.noreply.github.com>
2023-03-04 20:52:49 +0200

    Add project

---
+++ src/main/java/com/votemetric/biometricchoice/BiometricChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.votemetric.biometricchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometricChoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiometricChoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/votemetric/biometricchoice/BiometricChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votemetric.biometricchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricChoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
